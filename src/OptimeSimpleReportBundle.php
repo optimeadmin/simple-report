5a50fc172ff34e49523c6c2035ab51af6cee327e
Oscar Hernandez <ohernandez@optimeconsulting.com>
2022-09-09 16:58:55 -0400

    Archivo base bundle

---
+++ src/OptimeSimpleReportBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Optime\SimpleReport;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+
+class OptimeSimpleReportBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+
+    }
+}
