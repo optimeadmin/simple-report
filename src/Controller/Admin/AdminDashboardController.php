4dd3f939096d471a9839e9749d8e6d4a0905d0a3
Oscar Hernandez <o.j.hernandezf@gmail.com>
2022-09-15 15:55:48 -0400

    Cambio de ruta para el dashboard de simple report

--- src/Controller/Admin/AdminDashboardController.php
+++ src/Controller/Admin/AdminDashboardController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdminDashboardController extends AbstractDashboardController
 {
-    #[Route('/simplereportadmin', name: 'admin')]
+    #[Route('/simplereport/admin', name: 'admin')]
     public function index(): Response
     {
         //return parent::index();
